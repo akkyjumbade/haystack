e2dd7dcf2c3ccfeb1fefb104147d014c5adbde6c
vaibhavsutar264 <105624955+vaibhavsutar264@users.noreply.github.com>
2022-06-23 10:54:38 +0530

    updated about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const About = () => {
     return (
         <div className='center'>
-        <h2>About page</h2>
+        <h2>About page shown</h2>
             
         </div>
     )
